382d41dc40d5e15bf6dd60fe2dd4c46c34a721e1
Hannah Kim <hannahkm@fb.com>
2021-07-13 15:10:04 -0400

    started dashboard activity
    added recycler view and adapter for itineraries on the dashboard

--- app/src/main/java/com/example/fbufinalapp/DashboardFragment.java
+++ app/src/main/java/com/example/fbufinalapp/DashboardFragment.java
@@ -1,12 +1,27 @@
 package com.example.fbufinalapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.fbufinalapp.adapters.ItineraryAdapter;
+import com.example.fbufinalapp.models.Itinerary;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,34 +29,18 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DashboardFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Context context;
+    RecyclerView rvItineraries;
+    List<Itinerary> trips;
+    ItineraryAdapter adapter;
 
     public DashboardFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
+    public static DashboardFragment newInstance() {
         DashboardFragment fragment = new DashboardFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +48,48 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        context = container.getContext();
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvItineraries = view.findViewById(R.id.rvItineraries);
+        trips = new ArrayList<>();
+        adapter = new ItineraryAdapter(context, trips);
+
+        rvItineraries.setAdapter(adapter);
+        rvItineraries.setLayoutManager(new LinearLayoutManager(context));
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Itinerary> query = ParseQuery.getQuery(Itinerary.class);
+        query.include(Itinerary.KEY_USER);
+        // limit query to latest 20 items
+        // order posts by creation date (newest first)
+        query.addDescendingOrder("createdAt");
+        query.findInBackground(new FindCallback<Itinerary>() {
+            @Override
+            public void done(List<Itinerary> itineraries, ParseException e) {
+                if (e != null){
+                    Log.e("DashboardFragment", String.valueOf(e));
+                } else {
+                    // save received posts to list and notify adapter of new data
+                    trips.addAll(itineraries);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/fbufinalapp/models/Itinerary.java
@@ -0,0 +1,74 @@
+package com.example.fbufinalapp.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//@Parcel(analyze = Itinerary.class)
+@ParseClassName("Itinerary")
+public class Itinerary extends ParseObject {
+    public static String KEY_DESCRIPTION = "description";
+    public static String KEY_TITLE = "title";
+    public static String KEY_USER = "authors";
+    public static String KEY_PLACEID = "placeID";
+    public static String KEY_STARTDATE = "startDate";
+    public static String KEY_ENDDATE = "endDate";
+
+    public String getTitle() {
+        return getString(KEY_TITLE);
+    }
+
+    public void setTitle(String title){
+        put(KEY_TITLE, title);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String descrip){
+        put(KEY_DESCRIPTION, descrip);
+    }
+
+    public ParseUser getAuthor() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setAuthor(ParseUser parseUser){
+        put(KEY_USER, parseUser);
+    }
+
+    public String getPlaceID() {
+        return getString(KEY_PLACEID);
+    }
+
+    public void setPlaceID(String id){
+        put(KEY_PLACEID, id);
+    }
+
+    public Date getStartDate(){
+        return getDate(KEY_STARTDATE);
+    }
+
+    public void setStartDate(Date start){
+        put(KEY_STARTDATE, start);
+    }
+
+    public Date getEndDate(){
+        return getDate(KEY_ENDDATE);
+    }
+
+    public void setEndDate(Date end){
+        put(KEY_ENDDATE, end);
+    }
+
+    public String reformatDate(Date date){
+        String pattern = "MMMMM dd, yyyy";
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        return dateFormat.format(date);
+    }
+}
